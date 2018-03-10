83c0a4f8d7fc4d1ff2df5ab384b06de4522c2d2b
gmanjames <yms1465@gmail.com>
2018-03-10 16:46:22 -0600

    Added body-parser. Start begin page.

--- app.js
+++ app.js
@@ -2,21 +2,26 @@
 
 require('dotenv').config();
 
-const express = require('express'),
-      app     = express();
+const express    = require('express'),
+      app        = express();
+const bodyParser = require('body-parser')
 
 const DEFAULT_PORT = process.env.DEFAULT_PORT;
 
 
 app.use('/public', express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'pug');
 
 
 // APPLICATION ROUTES
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('begin');
 });
 
 
 // FIRE UP APPLICATION
-app.listen(DEFAULT_PORT, () => console.log('Listening on port 3000!'))
+app.listen(DEFAULT_PORT, () => console.log('Listening on port 3000!'));
