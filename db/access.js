33b5a2f4e77048883f7c3b511cd86004f900230c
gmanjames <yms1465@gmail.com>
2018-03-12 11:24:53 -0500

    Added slightly more intelligent mysql-con handeling.

--- db/access.js
+++ db/access.js
@@ -2,21 +2,32 @@ require('dotenv').config();
 
 
 const mysql = require('mysql');
-const con   = mysql.createConnection({
+const opts  = {
     host :      process.env.DB_HOST,
     user :      process.env.DB_USERNAME,
     password  : process.env.DB_PASSWORD,
     database  : process.env.DB
+};
 
-})
+function connection() {
+    return mysql.createConnection({
+        host :      process.env.DB_HOST,
+        user :      process.env.DB_USERNAME,
+        password  : process.env.DB_PASSWORD,
+        database  : process.env.DB
+
+    });
+}
 
 const dao = {};
 
 
 dao.findUser = function(username) {
     return new Promise((resolve, reject) => {
+        const con = connection();
         con.query("SELECT * FROM user WHERE user.username = '" + username + "'",
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
@@ -27,8 +38,10 @@ dao.findUser = function(username) {
 
 dao.saveUser = function(username, password_hash) {
   return new Promise((resolve, reject) => {
+        const con = connection();
         con.query("INSERT INTO user (username, password) values ('" + username + "', '" + password_hash + "')",
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
@@ -39,8 +52,10 @@ dao.saveUser = function(username, password_hash) {
 
 dao.allUsers = function() {
   return new Promise((resolve, reject) => {
+        const con = connection();
         con.query("SELECT username FROM user",
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
@@ -51,6 +66,7 @@ dao.allUsers = function() {
 
 dao.savePost = function (username, content) {
     return new Promise((resolve, reject) => {
+        const con = connection();
         let mm, dd, yyyy, date_str, date = new Date();
         mm   = date.getMonth() + 1;
         dd   = date.getDate();
@@ -58,6 +74,7 @@ dao.savePost = function (username, content) {
         date_str = yyyy + '-' + mm + '-' + dd;
         con.query("INSERT INTO post (user_username, content, posted) values ('" + username + "', '" + content + "', '" + date_str + "')",
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
@@ -68,8 +85,10 @@ dao.savePost = function (username, content) {
 
 dao.allPosts = function () {
   return new Promise((resolve, reject) => {
+        const con = connection();
         con.query("SELECT * FROM post ORDER BY id DESC",
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
@@ -80,8 +99,10 @@ dao.allPosts = function () {
 
 dao.likesForPost = function (post_id) {
   return new Promise((resolve, reject) => {
+        const con = connection();
         con.query("SELECT * FROM post_like WHERE post_id = " + post_id,
             function (error, results, fields) {
+                con.destroy();
                 if (error) reject(error);
 
                 resolve(results);
