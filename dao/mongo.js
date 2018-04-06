f64ca2d3186f9d4b1945ab3ad72af338bf57d7dc
gmanjames <yms1465@gmail.com>
2018-04-06 08:38:42 -0500

    Added some mongoose tests.
    Finished mongodb auth setup (for now).
    Plug everything into one file or now.

--- dao/mongo.js
+++ dao/mongo.js
@@ -1,3 +1,43 @@
+// Has to be a better way than this
+require('dotenv').config({ path: '../.env' });
+
 const mongoose = require('mongoose');
 
-const userSchema = require('../models/userSchema.js');
+// Connect to database:
+//     need to rethink connecion string bussiness.
+mongoose.connect(process.env.CONNECTION_STRING).then(
+    () => {
+        console.log('ok yes?');
+    },
+    (error) => {
+        console.log('boo');
+    }
+);
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    date_created: Date
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+const kiriyama = new User({ username: 'kiriyama', email: 'todo@todo.com', password: 'test', date_created: Date.now() });
+
+console.log(kiriyama);
